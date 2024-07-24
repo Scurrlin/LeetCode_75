992170ce0afb77d6c107ef4f288cc5c695656b85
Sean Currlin <sean.currlin@gmail.com>
2024-07-24 14:28:03 -0400

    Binary_Tree_DFS_reformat

--- Binary_Tree_DFS/1372_Longest_ZigZag.py
+++ Binary_Tree_DFS/1372_Longest_ZigZag.py
@@ -10,3 +10,19 @@
 # single node has a length of 0).
 
 # Return the longest ZigZag path contained in that tree.
+
+class Solution:
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+        self.maxi = 0
+
+        def dfs(node, left, right):
+            self.maxi = max(self.maxi, left, right)
+
+            if node.left:
+                dfs(node.left, right + 1, 0)
+
+            if node.right:
+                dfs(node.right, 0, left + 1)
+
+        dfs(root, 0, 0)
+        return self.maxi
--- Binary_Tree_DFS/872_Leaf-Similar_Trees.py
+++ Binary_Tree_DFS/872_Leaf-Similar_Trees.py
@@ -1,3 +1,12 @@
+# Consider all the leaves of a binary tree, from left to right order, the values
+# of those leaves form a leaf value sequence.
+
+# Two binary trees are considered leaf-similar if their leaf value sequence is
+# the same.
+
+# Return true if and only if the two given trees with head nodes root1 and root2
+# are leaf-similar.
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
