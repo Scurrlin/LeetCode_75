5f84ff850c25cc0fe355834ef2ffe1e881b17db3
Sean Currlin <sean.currlin@gmail.com>
2024-07-24 15:18:46 -0400

    Two_Pointers reformat

--- Two_Pointers/1679_Max_Num_K-Sum_Pairs.py
+++ Two_Pointers/1679_Max_Num_K-Sum_Pairs.py
@@ -1,3 +1,10 @@
+# You are given an integer array nums and an integer k.
+
+# In one operation, you can pick two numbers from the array whose sum equals k
+# and remove them from the array.
+
+# Return the maximum number of operations you can perform on the array.
+
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         nums.sort()
--- Two_Pointers/392_Is_Subsequence.py
+++ Two_Pointers/392_Is_Subsequence.py
@@ -5,3 +5,12 @@
 # string by deleting some (can be none) of the characters without disturbing the
 # relative positions of the remaining characters. (i.e., "ace" is a subsequence
 # of "abcde" while "aec" is not).
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return i == len(s)
