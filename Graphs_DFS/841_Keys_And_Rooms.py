b06be7a9117a99ddea7bbedcfaf08fe45fc1a2db
Sean Currlin <sean.currlin@gmail.com>
2024-07-24 14:48:04 -0400

    Graphs_DFS reformat

--- Graphs_DFS/841_Keys_And_Rooms.py
+++ Graphs_DFS/841_Keys_And_Rooms.py
@@ -9,3 +9,15 @@
 # Given an array rooms where rooms[i] is the set of keys that you can obtain if
 # you visited room i, return true if you can visit all the rooms, or false
 # otherwise.
+
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        k = new = set(rooms[0])
+        while len(k) < len(rooms) - 1:
+            new = reduce(
+                lambda a, b: a.union(b), (set(rooms[n]) for n in new), set()
+            ) - k.union({0})
+            if not new:
+                return False
+            k.update(new)
+        return True
