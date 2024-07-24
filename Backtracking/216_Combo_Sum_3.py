95fa70304ef73d8f3802eefc8fbf9f84a43dffb1
Sean Currlin <sean.currlin@gmail.com>
2024-07-24 10:28:33 -0400

    Backtracking reformat

--- Backtracking/216_Combo_Sum_3.py
+++ Backtracking/216_Combo_Sum_3.py
@@ -1,3 +1,10 @@
+# Find all valid combinations of k numbers that sum up to n such that the
+# following conditions are true:
+
+# Only numbers 1 through 9 are used. Each number is used at most once. Return a
+# list of all possible valid combinations. The list must not contain the same
+# combination twice, and the combinations may be returned in any order.
+
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         ans = []
