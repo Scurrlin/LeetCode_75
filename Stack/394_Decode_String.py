a2689083cc48533b6d827dda3caa07ec796c96b1
Sean Currlin <sean.currlin@gmail.com>
2024-07-24 15:14:24 -0400

    Stack reformat

--- Stack/394_Decode_String.py
+++ Stack/394_Decode_String.py
@@ -11,3 +11,24 @@
 
 # The test cases are generated so that the length of the output will never
 # exceed 105.
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+
+        for char in s:
+            if char is not "]":
+                stack.append(char)
+            else:
+                sub_str = ""
+                while stack[-1] is not "[":
+                    sub_str = stack.pop() + sub_str
+                stack.pop()
+
+                multiplier = ""
+                while stack and stack[-1].isdigit():
+                    multiplier = stack.pop() + multiplier
+
+                stack.append(int(multiplier) * sub_str)
+
+        return "".join(stack)
