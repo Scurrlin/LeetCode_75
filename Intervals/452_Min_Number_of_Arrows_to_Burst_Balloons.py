af774a5f358a3af49f91bd7fdd6976b833867e95
Sean Currlin <sean.currlin@gmail.com>
2024-07-24 14:59:32 -0400

    Intervals reformat

--- Intervals/452_Min_Number_of_Arrows_to_Burst_Balloons.py
+++ Intervals/452_Min_Number_of_Arrows_to_Burst_Balloons.py
@@ -12,3 +12,19 @@
 
 # Given the array points, return the minimum number of arrows that must be shot
 # to burst all balloons.
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort()
+
+        res = len(points)
+        prev = points[0]
+        for i in range(1, len(points)):
+            curr = points[i]
+            if curr[0] <= prev[1]:
+                res -= 1
+                prev = [curr[0], min(curr[1], prev[1])]
+            else:
+                prev = curr
+            
+        return res
