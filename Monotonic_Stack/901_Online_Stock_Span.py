47fde72ec3f454251b0cbdad80f320116d049be4
Sean Currlin <sean.currlin@gmail.com>
2024-07-24 15:05:15 -0400

    Monotonic_Stack reformat

--- Monotonic_Stack/901_Online_Stock_Span.py
+++ Monotonic_Stack/901_Online_Stock_Span.py
@@ -15,3 +15,15 @@
 
 # StockSpanner() Initializes the object of the class. int next(int price)
 # Returns the span of the stock's price given that today's price is price.
+
+class StockSpanner:
+    def __init__(self):
+        self.stack = []  # pair: (price, span)
+
+    def next(self, price: int) -> int:
+        span = 1
+        while self.stack and self.stack[-1][0] <= price:
+            span += self.stack[-1][1]
+            self.stack.pop()
+        self.stack.append((price, span))
+        return span
