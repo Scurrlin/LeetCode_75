3832293bddee1156467f4a3955d3d1cc00d6fa1e
Sean Currlin <sean.currlin@gmail.com>
2024-07-24 14:21:06 -0400

    Binary_Tree_BFS reformat

--- Binary_Tree_BFS/199_Binary_Tree_Right.py
+++ Binary_Tree_BFS/199_Binary_Tree_Right.py
@@ -1,3 +1,6 @@
+# Given the root of a binary tree, imagine yourself standing on the right side
+# of it, return the values of the nodes you can see ordered from top to bottom.
+
 class Solution:
     def rightSideView(self, root: TreeNode) -> List[int]:
         res = []
