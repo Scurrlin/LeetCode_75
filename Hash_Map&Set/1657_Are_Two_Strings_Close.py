f0b573e915b5ea0aab086aa69de90930f088e1fe
Sean Currlin <sean.currlin@gmail.com>
2024-07-24 14:51:17 -0400

    Hash_Map&Set reformat

--- Hash_Map&Set/1657_Are_Two_Strings_Close.py
+++ Hash_Map&Set/1657_Are_Two_Strings_Close.py
@@ -10,3 +10,22 @@
 
 # Given two strings, word1 and word2, return true if word1 and word2 are close,
 # and false otherwise.
+
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2):
+            return False
+
+        freq1 = [0] * 26
+        freq2 = [0] * 26
+
+        for ch in word1:
+            freq1[ord(ch) - ord('a')] += 1
+        for ch in word2:
+            freq2[ord(ch) - ord('a')] += 1
+
+        for f1, f2 in zip(freq1, freq2):
+            if (f1 == 0) != (f2 == 0):
+                return False
+
+        return sorted(freq1) == sorted(freq2)
