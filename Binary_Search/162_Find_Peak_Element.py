df7baeba36c159c3fbb218fc56b7d29706eb5791
Sean Currlin <sean.currlin@gmail.com>
2025-01-18 09:03:05 -0500

    update solution for 162

--- Binary_Search/162_Find_Peak_Element.py
+++ Binary_Search/162_Find_Peak_Element.py
@@ -14,11 +14,11 @@ class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         l, r = 0, len(nums) - 1
         while l <= r:
-            mid = (r + l) // 2
-            if mid < len(nums) - 1 and nums[mid] < nums[mid+1]:
-                l = mid + 1
-            elif mid > 0 and nums[mid] < nums[mid-1]:
-                r = mid - 1
+            m = (r + l) // 2
+            if m < len(nums) - 1 and nums[m] < nums[m + 1]:
+                l = m + 1
+            elif m > 0 and nums[m] < nums[m - 1]:
+                r = m - 1
             else:
                 break
-        return mid
+        return m
