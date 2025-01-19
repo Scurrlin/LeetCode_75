aa9281f69526e712758af924604c574d558a5b2d
Sean Currlin <sean.currlin@gmail.com>
2025-01-19 11:31:17 -0500

    update solution for 437

--- Binary_Tree_DFS/437_Path_Sum_III.py
+++ Binary_Tree_DFS/437_Path_Sum_III.py
@@ -4,24 +4,21 @@
 # The path does not need to start or end at the root or a leaf, but it must go
 # downwards (i.e., traveling only from parent nodes to child nodes).
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
 class Solution:
     def pathSum(self, root: TreeNode, targetSum: int) -> int:
-        def dfs(node: TreeNode, current_sum: int, target: int, prefix_sums: dict[int, int]) -> int:
+        def dfs(node: TreeNode, currentSum: int, target: int, prefixSums: dict[int, int]) -> int:
+            cSum = currentSum
+            pSums = prefixSums
+            
             if not node:
                 return 0
-            current_sum += node.val
-            count = prefix_sums.get(current_sum - target, 0)
-            prefix_sums[current_sum] = prefix_sums.get(current_sum, 0) + 1
-            count += dfs(node.left, current_sum, target, prefix_sums)
-            count += dfs(node.right, current_sum, target, prefix_sums)
-            prefix_sums[current_sum] -= 1
+            cSum += node.val
+            count = pSums.get(cSum - target, 0)
+            pSums[cSum] = pSums.get(cSum, 0) + 1
+            count += dfs(node.left, cSum, target, pSums)
+            count += dfs(node.right, cSum, target, pSums)
+            pSums[cSum] -= 1
             return count
         
-        prefix_sums = {0: 1}
-        return dfs(root, 0, targetSum, prefix_sums)
+        pSums = {0: 1}
+        return dfs(root, 0, targetSum, pSums)
