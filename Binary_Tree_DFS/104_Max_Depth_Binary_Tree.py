d6ce4680e8f9591351cd72d8c361b87c82cdd4dc
Sean Currlin <sean.currlin@gmail.com>
2025-01-19 11:23:48 -0500

    update solution for 104

--- Binary_Tree_DFS/104_Max_Depth_Binary_Tree.py
+++ Binary_Tree_DFS/104_Max_Depth_Binary_Tree.py
@@ -3,15 +3,12 @@
 # A binary tree's maximum depth is the number of nodes along the longest path
 # from the root node down to the farthest leaf node.
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
         if not root:
             return 0
-
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+        
+        return max(left_depth, right_depth) + 1
