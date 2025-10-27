d8eafb5179b577ba64566ff80366e12a68bad677
Sean Currlin <sean.currlin@gmail.com>
2025-10-27 17:38:38 -0400

    update solution for 394

--- Stack/394_Decode_String.py
+++ Stack/394_Decode_String.py
@@ -15,20 +15,18 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         stack = []
-
         for char in s:
             if char is not "]":
                 stack.append(char)
             else:
-                sub_str = ""
-                while stack[-1] is not "[":
-                    sub_str = stack.pop() + sub_str
-                stack.pop()
+                subStr = ""
 
+                while stack[-1] is not "[":
+                    subStr = stack.pop() + subStr
+                stack.pop()
                 multiplier = ""
                 while stack and stack[-1].isdigit():
                     multiplier = stack.pop() + multiplier
-
-                stack.append(int(multiplier) * sub_str)
+                stack.append(int(multiplier) * subStr)
 
         return "".join(stack)
