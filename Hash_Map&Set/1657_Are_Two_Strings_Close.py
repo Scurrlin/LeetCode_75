19c95bea03874dcf1c9f90d2fbf26a8d2f2631ef
Sean Currlin <sean.currlin@gmail.com>
2025-10-27 16:22:52 -0400

    update solution for 1657

--- Hash_Map&Set/1657_Are_Two_Strings_Close.py
+++ Hash_Map&Set/1657_Are_Two_Strings_Close.py
@@ -15,7 +15,6 @@ class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         if len(word1) != len(word2):
             return False
-
         freq1 = [0] * 26
         freq2 = [0] * 26
 
@@ -23,7 +22,6 @@ class Solution:
             freq1[ord(ch) - ord('a')] += 1
         for ch in word2:
             freq2[ord(ch) - ord('a')] += 1
-
         for f1, f2 in zip(freq1, freq2):
             if (f1 == 0) != (f2 == 0):
                 return False
