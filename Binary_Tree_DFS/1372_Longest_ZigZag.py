047755b917f50085656c198a6e1baba6ea44a9e9
Sean Currlin <sean.currlin@gmail.com>
2025-10-27 11:46:42 -0400

    update solution for 1372

--- Binary_Tree_DFS/1372_Longest_ZigZag.py
+++ Binary_Tree_DFS/1372_Longest_ZigZag.py
@@ -13,16 +13,15 @@
 
 class Solution:
     def longestZigZag(self, root: Optional[TreeNode]) -> int:
-        self.maxi = 0
+        self.maximum = 0
 
         def dfs(node, left, right):
-            self.maxi = max(self.maxi, left, right)
-
+            self.maximum = max(self.maximum, left, right)
+            
             if node.left:
                 dfs(node.left, right + 1, 0)
-
             if node.right:
                 dfs(node.right, 0, left + 1)
 
         dfs(root, 0, 0)
-        return self.maxi
+        return self.maximum
