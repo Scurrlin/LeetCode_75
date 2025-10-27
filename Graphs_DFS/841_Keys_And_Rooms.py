d21d68661f8b2c13b242d25dce658bd63f2796fe
Sean Currlin <sean.currlin@gmail.com>
2025-10-27 13:25:13 -0400

    update solution for 841

--- Graphs_DFS/841_Keys_And_Rooms.py
+++ Graphs_DFS/841_Keys_And_Rooms.py
@@ -12,12 +12,15 @@
 
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        k = new = set(rooms[0])
-        while len(k) < len(rooms) - 1:
-            new = reduce(
-                lambda a, b: a.union(b), (set(rooms[n]) for n in new), set()
-            ) - k.union({0})
-            if not new:
-                return False
-            k.update(new)
-        return True
+        size = len(rooms)
+        visited = [False] * size
+
+        def dfs(room):
+            if visited[room]:
+                return 
+            visited[room] = True
+            for key in rooms[room]:
+                dfs(key)
+        
+        dfs(0)
+        return all(visited)
