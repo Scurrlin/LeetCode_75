37ab1bda1eebb6a9170047b757fff549d1accd9a
Sean Currlin <sean.currlin@gmail.com>
2025-10-27 16:41:41 -0400

    update solution 2352

--- Hash_Map&Set/2352_Equal_Row_and_Column_Pairs.py
+++ Hash_Map&Set/2352_Equal_Row_and_Column_Pairs.py
@@ -6,14 +6,13 @@
 
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        m = defaultdict(int)
+        matrix = defaultdict(int)
         count = 0
 
         for row in grid:
-            m[str(row)] += 1
-        
+            matrix[str(row)] += 1
         for i in range(len(grid[0])):
             column = [grid[j][i] for j in range(len(grid))]
-            count += m[str(column)]
+            count += matrix[str(column)]
         
         return count
