5e20cb7805cba1c7d2032b6088051fb043b9b36d
Sean Currlin <sean.currlin@gmail.com>
2025-10-27 16:59:16 -0400

    update solution for 328

--- Linked_Lists/328_Odd_Even_LL.py
+++ Linked_Lists/328_Odd_Even_LL.py
@@ -14,17 +14,14 @@ class Solution:
     def oddEvenList(self, head):
         if not head or not head.next:
             return head
-        
         odd = odd_ptr = ListNode(0)
         even = even_ptr = ListNode(0)
         
         while head:
             odd_ptr.next = head
             even_ptr.next = head.next
-            
             odd_ptr = odd_ptr.next
             even_ptr = even_ptr.next
-            
             head = head.next.next if even_ptr else None
         
         odd_ptr.next = even.next
