71c7bc7d2a73ac929738e3b016b708a2ee7c2696
Sean Currlin <sean.currlin@gmail.com>
2025-10-27 17:47:58 -0400

    update solutfion for 208

--- Trie/208_Implement_Trie(Prefix_Tree).py
+++ Trie/208_Implement_Trie(Prefix_Tree).py
@@ -11,37 +11,29 @@
 # inserted string word that has the prefix prefix, and false otherwise.
 
 class Trie:
-
     def __init__(self):
-        self.root={}
+        self.root = {}
         
     def insert(self, word: str) -> None:
-
-        curr=self.root
-
+        curr = self.root
         for letter in word:
             if letter not in curr:
-                curr[letter]={}
-            curr=curr[letter]
-
-        curr['*']=''
+                curr[letter] = {}
+            curr = curr[letter]
+        curr['*'] = ''
 
     def search(self, word: str) -> bool:
-
-        curr=self.root
+        curr = self.root
         for letter in word:
             if letter not in curr:
                 return False
-            curr=curr[letter]
-
+            curr = curr[letter]
         return '*' in curr
         
     def startsWith(self, prefix: str) -> bool:
-
-        curr=self.root
+        curr = self.root
         for letter in prefix:
             if letter not in curr:
                 return False
-            curr=curr[letter]
-
+            curr = curr[letter]
         return True
