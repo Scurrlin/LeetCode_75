d96766bad3fd4c69bfdc566223094f2de7b06697
Sean Currlin <sean.currlin@gmail.com>
2025-10-27 11:55:35 -0400

    update solution for 62

--- DP-MD/62_Unique_Paths.py
+++ DP-MD/62_Unique_Paths.py
@@ -12,7 +12,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         row = [1] * n
-
         for i in range(m - 1):
             newRow = [1] * n
             for j in range(n - 2, -1, -1):
