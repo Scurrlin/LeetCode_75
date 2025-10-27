6f3363206aa017b81c6d652cfa08ecebb5126d6e
Sean Currlin <sean.currlin@gmail.com>
2025-10-27 16:57:14 -0400

    update solution for 452

--- Intervals/452_Min_Number_of_Arrows_to_Burst_Balloons.py
+++ Intervals/452_Min_Number_of_Arrows_to_Burst_Balloons.py
@@ -16,9 +16,8 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         points.sort()
+        res, prev = len(points), points[0]
 
-        res = len(points)
-        prev = points[0]
         for i in range(1, len(points)):
             curr = points[i]
             if curr[0] <= prev[1]:
@@ -26,5 +25,5 @@ class Solution:
                 prev = [curr[0], min(curr[1], prev[1])]
             else:
                 prev = curr
-            
+        
         return res
