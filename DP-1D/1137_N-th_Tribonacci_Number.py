77ca2ed8e336d7f8ffeb5717b453acd0b1af5775
Sean Currlin <sean.currlin@gmail.com>
2025-10-27 11:54:26 -0400

    update solution for 1137

--- DP-1D/1137_N-th_Tribonacci_Number.py
+++ DP-1D/1137_N-th_Tribonacci_Number.py
@@ -6,14 +6,10 @@
 
 class Solution:
     def tribonacci(self, n: int) -> int:
-
-        t0 =0
-        t1= 1
-        t2 =1
+        t0, t1, t2 = 0, 1, 1
         for i in range(n):
             temp = t0
             t0 = t1
             t1 = t2
             t2 = temp + t0 + t1
-            
         return t0
