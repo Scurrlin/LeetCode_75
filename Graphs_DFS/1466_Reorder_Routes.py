024df993d705c812a38a9c7a9f7aed95b45606a9
Sean Currlin <sean.currlin@gmail.com>
2025-10-27 13:29:32 -0400

    update solution for 1466

--- Graphs_DFS/1466_Reorder_Routes.py
+++ Graphs_DFS/1466_Reorder_Routes.py
@@ -15,20 +15,18 @@
 # It's guaranteed that each city can reach city 0 after reorder.
 
 class Solution:
-    def dfs(self, adj: List[List[Tuple[int, int]]], visited: List[bool], minChange: List[int], currCity: int) -> None:
-        visited[currCity] = True
-        for neighbourCity in adj[currCity]:
-            if not visited[neighbourCity[0]]:
-                if neighbourCity[1] == 1:
-                    minChange[0] += 1
-                self.dfs(adj, visited, minChange, neighbourCity[0])
-    
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        adj = [[] for _ in range(n)]
-        for connection in connections:
-            adj[connection[0]].append((connection[1], 1))
-            adj[connection[1]].append((connection[0], -1))
-        visited = [False] * n
-        minChange = [0]
-        self.dfs(adj, visited, minChange, 0)
-        return minChange[0]
+        visited, visited[0] = [False] * n, True
+        ret, temp = 0, []
+        while connections:
+            for start, end in connections:
+                if visited[end]:
+                    visited[start] = True
+                elif visited[start]:
+                    ret += 1
+                    visited[end] = True
+                else:
+                    temp.append([start,end])
+            connections = temp[::-1]
+            temp.clear()
+        return ret
