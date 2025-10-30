47fae034caafca6b2adffc60bb5f6970fcd77cfd
Sean Currlin <sean.currlin@gmail.com>
2025-10-29 23:23:55 -0400

    update solution for 238

--- Arrays&Strings/238_Product_of_Array_Except_Self.py
+++ Arrays&Strings/238_Product_of_Array_Except_Self.py
@@ -9,15 +9,16 @@
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        pre = [1] * n
-        suf = [1] * n
+        output = [1] * len(nums)
+        
+        left = 1
+        for i in range(len(nums)):
+            output[i] *= left
+            left *= nums[i]
+            
+        right = 1
+        for i in range(len(nums) - 1, -1, -1):
+            output[i] *= right
+            right *= nums[i]
 
-        for i in range(1, n):
-            pre[i] = pre[i - 1] * nums[i - 1]
-        for i in range(n - 2, -1, -1):
-            suf[i] = suf[i + 1] * nums[i + 1]
-        
-        answer = [pre[i] * suf[i] for i in range(n)]
-        
-        return answer
+        return output
